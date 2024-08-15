c744c3a31afd458d1bf4e5e3335f0b567da5ff72
aaronRosenfeld <49607188+aaronRosenfeld@users.noreply.github.com>
2024-08-15 01:19:48 -0400

    Initial Commit

---
+++ petal/petalApp.swift
@@ -0,0 +1,17 @@
+//
+//  petalApp.swift
+//  petal
+//
+//  Created by Aaron Rosenfeld on 8/15/24.
+//
+
+import SwiftUI
+
+@main
+struct petalApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
